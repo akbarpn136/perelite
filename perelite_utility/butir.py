2222930bbee355344326a81db39708f7a99016cd
akbarpn136 <akbarpn136@gmail.com>
2017-10-24 16:57:33 +0700

    membuat query awal document butir

--- perelite_utility/butir.py
+++ perelite_utility/butir.py
@@ -1,6 +1,18 @@
+from django.http import HttpResponse
 from rest_framework import generics
+from . import models
 
 
 # Create your views here.
 class Butir(generics.ListCreateAPIView):
-    pass
+    def get_queryset(self):
+        q = models.Butir.objects.to_json()
+
+        return q
+
+    def get(self, request, *args, **kwargs):
+        obj = self.get_queryset()
+        return HttpResponse(obj)
+
+    def post(self, request, *args, **kwargs):
+        pass
