92b6c7f683430303a5e9b134bc24b3eaac375c89
akbarpn136 <akbarpn136@gmail.com>
2017-10-24 16:20:19 +0700

    membuat model untuk kebutuhan personil dan butir kegiatan

--- perelite_utility/models.py
+++ perelite_utility/models.py
@@ -1,3 +1,51 @@
-from django.db import models
+from mongoengine import *
+
+OPSI_HASIL = (
+    'Lembar Kerja',
+    'Lembar Kerja/Logbook',
+    'Lembar Kerja/Foto',
+    'Benda Kerja/Lembar Kerja',
+    'Logbook',
+    'Surat Keputusan',
+    'Technical Note',
+)
+
+OPSI_PELAKSANA = (
+    'Perekayasa Pertama',
+    'Perekayasa Pertama/Muda',
+    'Perekayasa Muda',
+    'Perekayasa Muda/Madya',
+    'Perekayasa Madya',
+    'Perekayasa Madya/Utama',
+    'Perekayasa Utama',
+    'Semua Jenjang',
+)
+
 
 # Create your models here.
+class Butir(Document):
+    nama = StringField(required=True)
+    butir = StringField(max_length=120, required=True)
+    hasil = StringField(required=True, choices=OPSI_HASIL)
+    angka = FloatField(required=True)
+    pelaksana = StringField(required=True, choices=OPSI_PELAKSANA)
+    jenis = StringField(required=True, choices=('Pendidikan', 'Kerekayasaan', 'Profesi', 'Penunjang'))
+
+
+class Profil(Document):
+    nip = StringField(required=True)
+    pendidikan = StringField()
+    instansi = StringField()
+    singkatan = StringField()
+    pangkat = StringField(required=True)
+    jabatan = StringField(required=True)
+
+
+class Personil(Document):
+    username = StringField(required=True)
+    password = StringField(required=True)
+    token = StringField(required=True)
+    nama_depan = StringField()
+    nama_belakang = StringField()
+    email = EmailField(required=True)
+    profil = ReferenceField(Profil, reverse_delete_rule=CASCADE)
