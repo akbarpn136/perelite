216589ea38c9333ba31ed48123ba5e76aa008a80
akbarpn136 <akbarpn136@gmail.com>
2017-10-27 09:02:24 +0700

    membuat fungsi update dan delete butir kegiatan

--- perelite_utility/butir.py
+++ perelite_utility/butir.py
@@ -3,7 +3,9 @@ from rest_framework import generics
 from . import models
 import json
 from .paginations import Pagination
-from mongoengine import ValidationError, NotUniqueError
+from mongoengine import (ValidationError, NotUniqueError,
+                         DoesNotExist, MultipleObjectsReturned)
+# from django.contrib.auth import hashers
 
 
 # Create your views here.
@@ -36,7 +38,61 @@ class Butir(generics.ListCreateAPIView):
         except ValidationError as err:
             return HttpResponse(json.dumps(err.to_dict()))
 
-        except NotUniqueError as nu:
+        except NotUniqueError:
             return HttpResponse(json.dumps({
-                'butir': 'Tried to save duplicate unique keys'
+                'detail': 'Tried to save duplicate unique keys'
             }))
+
+
+class ButirModifikasi(generics.RetrieveUpdateDestroyAPIView):
+    def get_object(self):
+        try:
+            q = models.Butir.objects.get(butir=self.kwargs['butir'])
+            return q
+
+        except DoesNotExist:
+            return HttpResponse(json.dumps({
+                'detail': 'Matching query does not exist.'
+            }))
+
+        except MultipleObjectsReturned:
+            return HttpResponse(json.dumps({
+                'detail': 'Multiple object found'
+            }))
+
+    def get(self, request, *args, **kwargs):
+        butir = self.get_object()
+
+        if isinstance(butir, models.Butir):
+            return HttpResponse(butir.to_json())
+        else:
+            return butir
+
+    def put(self, request, *args, **kwargs):
+        butir = self.get_object()
+
+        if isinstance(butir, models.Butir):
+            butir.update(
+                set__nama=request.POST.get('nama'),
+                set__butir=request.POST.get('butir'),
+                set__hasil=request.POST.get('hasil'),
+                set__angka=request.POST.get('angka'),
+                set__pelaksana=request.POST.get('pelaksana'),
+                set__jenis=request.POST.get('jenis')
+            )
+
+            butir.reload()
+
+            return HttpResponse(butir.to_json())
+
+        else:
+            return butir
+
+    def destroy(self, request, *args, **kwargs):
+        butir = self.get_object()
+
+        if isinstance(butir, models.Butir):
+            butir.delete()
+            return HttpResponse(butir.to_json())
+        else:
+            return butir
