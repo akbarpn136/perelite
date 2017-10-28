781f39279f826136f7312d9bf1e0555e0efc31ec
akbarpn136 <akbarpn136@gmail.com>
2017-10-28 23:31:53 +0700

    membuat route untuk class pengaturan personil

--- perelite_utility/urls.py
+++ perelite_utility/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from . import butir
+from . import butir, personil
 
 urlpatterns = [
     url(r'^api1/butir/$', butir.Butir.as_view()),
     url(r'^api1/butir/(?P<butir>\w.+)/$', butir.ButirModifikasi.as_view()),
+
+    url(r'^api1/personil/$', personil.Personil.as_view()),
+    url(r'^api1/personil/(?P<username>\w+)/$', personil.PersonilModifikasi.as_view()),
 ]
