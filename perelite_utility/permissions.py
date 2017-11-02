7e6052a10750a4df5c3b1589873a5cd113a3ce4f
akbarpn136 <akbarpn136@gmail.com>
2017-11-02 20:44:59 +0700

    membuat penyesuaian otorisasi terhadap aktifitas pengguna aplikasi

---
+++ perelite_utility/permissions.py
@@ -0,0 +1,28 @@
+from rest_framework import exceptions
+from rest_framework import permissions
+from . import models
+
+
+def _user(username):
+    return models.Personil.objects.get(username=username)
+
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        try:
+            return _user(request.user.username).is_superuser()
+
+        except models.Personil.DoesNotExist:
+            raise exceptions.PermissionDenied()
+
+
+class IsAdminOrLimitedAuthenticated(permissions.BasePermission):
+    def has_permission(self, request, view):
+        try:
+            if _user(request.user.username).is_superuser():
+                return True
+            else:
+                return True if request.method == 'GET' else False
+
+        except models.Personil.DoesNotExist:
+            raise exceptions.PermissionDenied()
