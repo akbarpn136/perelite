d7d4c109feb6cc8c045846d581dce26430aa27df
akbarpn136 <akbarpn136@gmail.com>
2017-11-13 22:19:16 +0700

    mendaftarkan penerapan router

--- static/perelite_ui/src/main.js
+++ static/perelite_ui/src/main.js
@@ -1,7 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Router from 'vue-router';
+import {routes} from './routes';
+
+Vue.use(Router);
+
+const router = new Router({
+    routes
+});
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+    el: '#app',
+    router,
+    render: h => h(App)
+});
