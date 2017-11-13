6c7a3bd58f8313fb5c0925fd8418fe940f61e9cd
akbarpn136 <akbarpn136@gmail.com>
2017-11-13 22:18:47 +0700

    membuat pola url dengan menerapkan metoda lazy loading

---
+++ static/perelite_ui/src/routes.js
@@ -0,0 +1,18 @@
+import Utama from './components/Utama.vue';
+// import Pendidikan from './components/tugas/Pendidikan.vue';
+
+const Pendidikan = (resolve) => require(['./components/tugas/Pendidikan.vue'], resolve);
+const Kerekayasaan = (resolve) => require(['./components/tugas/Kerekayasaan.vue'], resolve);
+const Profesi = (resolve) => require(['./components/tugas/Profesi.vue'], resolve);
+const Penunjang = (resolve) => require(['./components/tugas/Penunjang.vue'], resolve);
+const Profil = (resolve) => require(['./components/pengaturan/Profil.vue'], resolve);
+
+export const routes = [
+    {path: '/', component: Utama, name: 'utama'},
+    {path: '/pendidikan', component: Pendidikan, name: 'pendidikan'},
+    {path: '/kerekayasaan', component: Kerekayasaan, name: 'kerekayasaan'},
+    {path: '/profesi', component: Profesi, name: 'profesi'},
+    {path: '/penunjang', component: Penunjang, name: 'penunjang'},
+    {path: '/profil', component: Profil, name: 'profil'},
+    {path: '**', redirect: '/'},
+];
