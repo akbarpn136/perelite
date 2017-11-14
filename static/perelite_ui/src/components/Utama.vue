03d93258b2797739f8fc3fe0e4148e36e0bd1df8
akbarpn136 <akbarpn136@gmail.com>
2017-11-14 20:42:17 +0700

    membuat banyak pengaturan layout halman utama aplikasi berupa ringkasan kegiatan

--- static/perelite_ui/src/components/Utama.vue
+++ static/perelite_ui/src/components/Utama.vue
@@ -1,5 +1,140 @@
 <template>
-    <div class="content">
-        <p>Halaman Utama</p>
+    <div>
+        <div class="columns is-desktop">
+            <div class="column">
+                <div class="box is-radiusless">
+                    <h1 class="title is-4 is-spaced">Ringkasan Kegiatan</h1>
+                    <h2 class="subtitle is-6">
+                        Ringkasan berupa total angka kegiatan selama tahun berjalan dan daftar yang dikerjakan
+                    </h2>
+                </div>
+            </div>
+        </div>
+        <div class="columns is-desktop">
+            <div class="column">
+                <div class="box is-radiusless is-paddingless">
+                    <nav class="panel">
+                        <p class="panel-heading">
+                            Pendidikan
+                        </p>
+                        <a class="panel-block">
+                            <span class="panel-icon">
+                                <i class="fa fa-book"></i>
+                            </span>
+                            bulma
+                        </a>
+
+                        <a class="panel-block">
+                            <span class="panel-icon">
+                                <i class="fa fa-book"></i>
+                            </span>
+                            marksheet
+                        </a>
+                        <div class="panel-block">
+                            <p class="has-text-grey-light">Total: 3.52</p>
+                        </div>
+                        <div class="panel-block">
+                            <button class="button is-primary is-fullwidth">
+                                <span class="icon"><i class="fa fa-plus-circle"></i></span>
+                                <span>Tambah</span>
+                            </button>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+            <div class="column">
+                <div class="box is-radiusless is-paddingless">
+                    <nav class="panel">
+                        <p class="panel-heading">
+                            Kerekayasaan
+                        </p>
+                        <a class="panel-block">
+                            <span class="panel-icon">
+                                <i class="fa fa-book"></i>
+                            </span>
+                            bulma
+                        </a>
+
+                        <a class="panel-block">
+                            <span class="panel-icon">
+                                <i class="fa fa-book"></i>
+                            </span>
+                            marksheet
+                        </a>
+                        <div class="panel-block">
+                            <p class="has-text-grey-light">Total: 3.52</p>
+                        </div>
+                        <div class="panel-block">
+                            <button class="button is-primary is-fullwidth">
+                                <span class="icon"><i class="fa fa-plus-circle"></i></span>
+                                <span>Tambah</span>
+                            </button>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+            <div class="column">
+                <div class="box is-radiusless is-paddingless">
+                    <nav class="panel">
+                        <p class="panel-heading">
+                            Profesi
+                        </p>
+                        <a class="panel-block">
+                            <span class="panel-icon">
+                                <i class="fa fa-book"></i>
+                            </span>
+                            bulma
+                        </a>
+
+                        <a class="panel-block">
+                            <span class="panel-icon">
+                                <i class="fa fa-book"></i>
+                            </span>
+                            marksheet
+                        </a>
+                        <div class="panel-block">
+                            <p class="has-text-grey-light">Total: 3.52</p>
+                        </div>
+                        <div class="panel-block">
+                            <button class="button is-primary is-fullwidth">
+                                <span class="icon"><i class="fa fa-plus-circle"></i></span>
+                                <span>Tambah</span>
+                            </button>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+            <div class="column">
+                <div class="box is-radiusless is-paddingless">
+                    <nav class="panel">
+                        <p class="panel-heading">
+                            Penunjang
+                        </p>
+                        <a class="panel-block">
+                            <span class="panel-icon">
+                                <i class="fa fa-book"></i>
+                            </span>
+                            bulma
+                        </a>
+
+                        <a class="panel-block">
+                            <span class="panel-icon">
+                                <i class="fa fa-book"></i>
+                            </span>
+                            marksheet
+                        </a>
+                        <div class="panel-block">
+                            <p class="has-text-grey-light">Total: 3.52</p>
+                        </div>
+                        <div class="panel-block">
+                            <button class="button is-primary is-fullwidth">
+                                <span class="icon"><i class="fa fa-plus-circle"></i></span>
+                                <span>Tambah</span>
+                            </button>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
