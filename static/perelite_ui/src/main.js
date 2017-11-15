3c79f2b686ea83bf3a3280696cf0a2df896b2656
akbarpn136 <akbarpn136@gmail.com>
2017-11-15 21:27:10 +0700

    menambahkan penggunaan plugin form validasi

--- static/perelite_ui/src/main.js
+++ static/perelite_ui/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 import Router from 'vue-router';
+import VeeValidate from 'vee-validate';
 import {routes} from './routes';
 
 Vue.use(Router);
+Vue.use(VeeValidate);
 
 const router = new Router({
     routes
