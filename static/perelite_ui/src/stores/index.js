746b311175faaaa7d03529c0631263a3656b162e
akbarpn136 <akbarpn136@gmail.com>
2017-11-17 22:17:07 +0700

    memperbaiki nilai default state untuk show parent component

--- static/perelite_ui/src/stores/index.js
+++ static/perelite_ui/src/stores/index.js
@@ -6,7 +6,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
     state: {
         tugas: 'Pendidikan',
-        showParent: true
+        showParent: ''
     },
     getters: {
         jenisTugas(state) {
