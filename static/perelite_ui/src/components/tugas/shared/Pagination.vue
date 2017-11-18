deeeb4a231171fe33843b7d4eb661ba1de927758
akbarpn136 <akbarpn136@gmail.com>
2017-11-18 16:57:24 +0700

    menambahkan komponen baru untuk kebutuhan pagination

---
+++ static/perelite_ui/src/components/tugas/shared/Pagination.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav class="pagination" role="navigation" aria-label="pagination">
+        <a class="pagination-previous">
+            <span class="icon"><i class="fa fa-angle-left"></i></span>
+        </a>
+        <a class="pagination-next">
+            <span class="icon"><i class="fa fa-angle-right"></i></span>
+        </a>
+        <ul class="pagination-list">
+        </ul>
+    </nav>
+</template>
+
+<script></script>
