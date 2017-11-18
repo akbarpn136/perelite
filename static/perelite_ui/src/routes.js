c724c08ab7d93f061ae37cffad077cac2f3a9283
akbarpn136 <akbarpn136@gmail.com>
2017-11-18 16:57:50 +0700

    penataan kembali route yang digunakan dalam aplikasi

--- static/perelite_ui/src/routes.js
+++ static/perelite_ui/src/routes.js
@@ -1,33 +1,17 @@
 import Utama from './components/Utama.vue';
 
-const Pendidikan = (resolve) => require(['./components/tugas/Pendidikan.vue'], resolve);
-const PendidikanTambah = (resolve) => require(['./components/tugas/managed/ManagePendidikan.vue'], resolve);
-
-const Kerekayasaan = (resolve) => require(['./components/tugas/Kerekayasaan.vue'], resolve);
-const KerekayasaanTambah = (resolve) => require(['./components/tugas/managed/ManageKerekayasaan.vue'], resolve);
-
-const Profesi = (resolve) => require(['./components/tugas/Profesi.vue'], resolve);
-const ProfesiTambah = (resolve) => require(['./components/tugas/managed/ManageProfesi.vue'], resolve);
-
-const Penunjang = (resolve) => require(['./components/tugas/Penunjang.vue'], resolve);
-const PenunjangTambah = (resolve) => require(['./components/tugas/managed/ManagePenunjang.vue'], resolve);
-
+const Tugas = (resolve) => require(['./components/tugas/Tugas.vue'], resolve);
+const TugasBaru = (resolve) => require(['./components/tugas/managed/ManageTugas.vue'], resolve);
 const Profil = (resolve) => require(['./components/pengaturan/Profil.vue'], resolve);
 
 export const routes = [
-    {path: '/', component: Utama, name: 'utama'},
-    {path: '/pendidikan', component: Pendidikan, name: 'pendidikan', children: [
-        {path: 'tambah', component: PendidikanTambah, name: 'pendidikanBaru'}
+    {path: '/', component: Utama, name: 'utama', children: [
+        {path: 'tugas', component: TugasBaru, name: 'tugasBaru'},
+        {path: 'pendidikan', component: Tugas, name: 'pendidikan'},
+        {path: 'kerekayasaan', component: Tugas, name: 'kerekayasaan'},
+        {path: 'profesi', component: Tugas, name: 'profesi'},
+        {path: 'penunjang', component: Tugas, name: 'penunjang'},
+        {path: 'profil', component: Profil, name: 'profil'},
     ]},
-    {path: '/kerekayasaan', component: Kerekayasaan, name: 'kerekayasaan', children: [
-        {path: 'tambah', component: KerekayasaanTambah, name: 'kerekayasaanBaru'}
-    ]},
-    {path: '/profesi', component: Profesi, name: 'profesi', children: [
-        {path: 'tambah', component: ProfesiTambah, name: 'profesiBaru'}
-    ]},
-    {path: '/penunjang', component: Penunjang, name: 'penunjang', children: [
-        {path: 'tambah', component: PenunjangTambah, name: 'penunjangBaru'}
-    ]},
-    {path: '/profil', component: Profil, name: 'profil'},
     {path: '**', redirect: {name: 'utama'}},
 ];
