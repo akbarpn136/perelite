da32b03905a64415997bfe1ed9ea9306f6a27c89
akbarpn136 <akbarpn136@gmail.com>
2017-11-18 16:57:08 +0700

    melakukan penamaan komponen dan penyesuaian layout pengaturan tugas

---
+++ static/perelite_ui/src/components/tugas/managed/ManageTugas.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="box">Manage Tugas</div>
+</template>
+
+<script>
+    export default {
+        created() {
+            this.$store.commit('setShowParent', false);
+        }
+    }
+</script>
