db0a34d2163e5047e6261e2affdaf6c03384b0bf
akbarpn136 <akbarpn136@gmail.com>
2017-11-18 16:59:20 +0700

    penamaan ulang komponen tugas

---
+++ static/perelite_ui/src/components/tugas/Tugas.vue
@@ -0,0 +1,18 @@
+<template>
+    <app-tasks></app-tasks>
+</template>
+
+<script>
+    import Sidebar from './shared/Sidebar.vue';
+    import Tasks from './shared/Tasks.vue';
+
+    export default {
+        components: {
+            appSidebar: Sidebar,
+            appTasks: Tasks,
+        },
+        created() {
+            this.$store.commit('setShowParent', false);
+        }
+    }
+</script>
