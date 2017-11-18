192f96d3370aa96bec0fc761d96dc287edf5a682
akbarpn136 <akbarpn136@gmail.com>
2017-11-18 17:03:18 +0700

    penataan layout serta fungsi-fungsi yang digunakan untuk halaman utama aplikasi

--- static/perelite_ui/src/components/Utama.vue
+++ static/perelite_ui/src/components/Utama.vue
@@ -1,140 +1,39 @@
 <template>
     <div>
-        <div class="columns is-desktop">
-            <div class="column">
-                <div class="box is-radiusless">
-                    <h1 class="title is-4 is-spaced">Ringkasan Kegiatan</h1>
-                    <h2 class="subtitle is-6">
-                        Ringkasan berupa total angka kegiatan selama tahun berjalan dan daftar yang dikerjakan
-                    </h2>
-                </div>
-            </div>
-        </div>
-        <div class="columns is-desktop">
-            <div class="column">
-                <div class="box is-radiusless is-paddingless">
-                    <nav class="panel">
-                        <p class="panel-heading is-radiusless">
-                            Pendidikan
-                        </p>
-                        <a class="panel-block">
-                            <span class="panel-icon">
-                                <i class="fa fa-book"></i>
-                            </span>
-                            bulma
-                        </a>
-
-                        <a class="panel-block">
-                            <span class="panel-icon">
-                                <i class="fa fa-book"></i>
-                            </span>
-                            marksheet
-                        </a>
-                        <div class="panel-block">
-                            <p class="has-text-grey-light">Total: 3.52</p>
-                        </div>
-                        <div class="panel-block">
-                            <button class="button is-primary is-fullwidth">
-                                <span class="icon"><i class="fa fa-plus-circle"></i></span>
-                                <span>Tambah</span>
-                            </button>
-                        </div>
-                    </nav>
-                </div>
-            </div>
-            <div class="column">
-                <div class="box is-radiusless is-paddingless">
-                    <nav class="panel">
-                        <p class="panel-heading is-radiusless">
-                            Kerekayasaan
-                        </p>
-                        <a class="panel-block">
-                            <span class="panel-icon">
-                                <i class="fa fa-book"></i>
-                            </span>
-                            bulma
-                        </a>
-
-                        <a class="panel-block">
-                            <span class="panel-icon">
-                                <i class="fa fa-book"></i>
-                            </span>
-                            marksheet
-                        </a>
-                        <div class="panel-block">
-                            <p class="has-text-grey-light">Total: 3.52</p>
-                        </div>
-                        <div class="panel-block">
-                            <button class="button is-primary is-fullwidth">
-                                <span class="icon"><i class="fa fa-plus-circle"></i></span>
-                                <span>Tambah</span>
-                            </button>
-                        </div>
-                    </nav>
-                </div>
-            </div>
-            <div class="column">
-                <div class="box is-radiusless is-paddingless">
-                    <nav class="panel">
-                        <p class="panel-heading is-radiusless">
-                            Profesi
-                        </p>
-                        <a class="panel-block">
-                            <span class="panel-icon">
-                                <i class="fa fa-book"></i>
-                            </span>
-                            bulma
-                        </a>
-
-                        <a class="panel-block">
-                            <span class="panel-icon">
-                                <i class="fa fa-book"></i>
-                            </span>
-                            marksheet
-                        </a>
-                        <div class="panel-block">
-                            <p class="has-text-grey-light">Total: 3.52</p>
-                        </div>
-                        <div class="panel-block">
-                            <button class="button is-primary is-fullwidth">
-                                <span class="icon"><i class="fa fa-plus-circle"></i></span>
-                                <span>Tambah</span>
-                            </button>
-                        </div>
-                    </nav>
-                </div>
-            </div>
-            <div class="column">
-                <div class="box is-radiusless is-paddingless">
-                    <nav class="panel">
-                        <p class="panel-heading is-radiusless">
-                            Penunjang
-                        </p>
-                        <a class="panel-block">
-                            <span class="panel-icon">
-                                <i class="fa fa-book"></i>
-                            </span>
-                            bulma
-                        </a>
-
-                        <a class="panel-block">
-                            <span class="panel-icon">
-                                <i class="fa fa-book"></i>
-                            </span>
-                            marksheet
-                        </a>
-                        <div class="panel-block">
-                            <p class="has-text-grey-light">Total: 3.52</p>
-                        </div>
-                        <div class="panel-block">
-                            <button class="button is-primary is-fullwidth">
-                                <span class="icon"><i class="fa fa-plus-circle"></i></span>
-                                <span>Tambah</span>
-                            </button>
-                        </div>
-                    </nav>
-                </div>
-            </div>
-        </div>
+        <div class="box is-radiusless" v-if="isParent" :key="$route.path">Something good.</div>
+        <transition enterActiveClass="animated slideInRight"
+                    leaveActiveClass="animated slideOutRight"
+                    mode="out-in"
+                    duration="650">
+            <router-view :key="$route.path"></router-view>
+        </transition>
     </div>
 </template>
+
+<script>
+    import Sidebar from './tugas/shared/Sidebar.vue';
+
+    export default {
+        components: {
+            appSidebar: Sidebar,
+        },
+        computed: {
+            isParent() {
+                return this.$store.getters.getShowParent;
+            }
+        },
+        created() {
+            this.$store.commit('setShowParent', true);
+        },
+        beforeRouteUpdate(to, from, next) {
+            if (to.name === 'utama') {
+                this.$store.commit('setShowParent', true);
+            }
+
+            else {
+                this.$store.commit('setShowParent', false);
+            }
+            next();
+        }
+    }
+</script>
