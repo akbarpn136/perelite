682f4188167bcc4d35dbceab7a2a5cacf3e5f281
akbarpn136 <akbarpn136@gmail.com>
2017-11-19 13:24:41 +0700

    memberikan translasi bahasa kesalahan validasi melalui data local

--- static/perelite_ui/src/main.js
+++ static/perelite_ui/src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue';
 import App from './App.vue';
 import Router from 'vue-router';
-import VeeValidate from 'vee-validate';
+import VeeValidate, {Validator} from 'vee-validate';
+import id from 'vee-validate/dist/locale/id';
 import {routes} from './routes';
 import store from './stores';
 
 Vue.use(Router);
-Vue.use(VeeValidate);
+
+Validator.localize('id', id);
+Vue.use(VeeValidate, {
+  locale: 'id'
+});
 
 const router = new Router({
     routes
