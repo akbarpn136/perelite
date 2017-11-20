ab0287ad4ae21ebe6b8f1f98c76fdd8144591bbf
akbarpn136 <akbarpn136@gmail.com>
2017-11-20 23:09:04 +0700

    memberikan pengaturan ESLint javascript beserta penambahan modul

--- static/quasar_ui/.eslintrc.js
+++ static/quasar_ui/.eslintrc.js
@@ -31,7 +31,7 @@ module.exports = {
     'import/named': 2,
     'import/namespace': 2,
     'import/default': 2,
-    'import/export': 2,
+    'import/export': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'brace-style': [2, 'stroustrup', { 'allowSingleLine': true }]
