56acc0dbb9ae4841a50999697236b233b86e21d4
akbarpn136 <akbarpn136@gmail.com>
2017-11-21 22:34:45 +0700

    menambahkan komponen baru sebagai helper untuk floating action

---
+++ static/quasar_ui/src/components/helper/FloatingAction.vue
@@ -0,0 +1,40 @@
+<template>
+    <q-fixed-position corner="bottom-right" :offset="[18, 18]">
+        <q-btn round color="green-5" small @click="onTugasClick">
+            <q-icon name="add"></q-icon>
+            <q-tooltip anchor="center left"
+                       self="center right"
+                       :offset="[20, 0]">Tambah Tugas
+            </q-tooltip>
+        </q-btn>
+    </q-fixed-position>
+</template>
+
+<script>
+    import {
+        QFixedPosition,
+        QBtn,
+        QIcon,
+        QTooltip
+    } from 'quasar';
+
+    export default {
+        data() {
+            return {}
+        },
+        components: {
+            QFixedPosition,
+            QBtn,
+            QIcon,
+            QTooltip
+        },
+        methods: {
+            onTugasClick() {
+                this.$store.commit('setShowModalTugas', true);
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
