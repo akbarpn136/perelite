743738c0de8ececf4b74ec7c574715e33b6dc187
akbarpn136 <akbarpn136@gmail.com>
2017-11-21 22:35:34 +0700

    menambahkan komponen baru sebagai helper untuk modal tugas

---
+++ static/quasar_ui/src/components/helper/ModalTugas.vue
@@ -0,0 +1,39 @@
+<template>
+    <q-modal v-model="modalStats" maximized :content-classes="['layout-padding', 'pt-3']">
+        <h4>Basic Modal</h4>
+        <q-btn color="primary"
+               @click="onModalClose">
+            Close
+        </q-btn>
+    </q-modal>
+</template>
+
+<script>
+    import {
+        QBtn,
+        QModal
+    } from 'quasar';
+
+    export default {
+        data() {
+            return {}
+        },
+        components: {
+            QBtn,
+            QModal
+        },
+        methods: {
+            onModalClose() {
+                this.$store.commit('setShowModalTugas', false);
+            }
+        },
+        computed: {
+            modalStats() {
+                return this.$store.getters.getShowModalTugas;
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
