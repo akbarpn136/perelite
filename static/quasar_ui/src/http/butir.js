d6e122ea1562872bcf9db7a38d2b1f31c29525fc
akbarpn136 <akbarpn136@gmail.com>
2017-11-25 19:00:10 +0700

    membuat fungsi untuk request butir

---
+++ static/quasar_ui/src/http/butir.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8000/' : 'http://bbta3.bppt.go.id:9601/';
+
+const QWERTY = JSON.parse(localStorage.getItem('qwerty')) || null;
+
+function GetButir(kategori=null) {
+    return axios.get(`${URL}utility/api1/butir/${kategori}/`, {
+        headers: {
+            'Authorization': `Bearer ${QWERTY.uid}`,
+            'Content-Type': 'application/json'
+        }
+    });
+}
+
+export {GetButir};
