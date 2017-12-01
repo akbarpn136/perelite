5bb16e35d323548eecd63dd19ae380a63d5a40b3
akbarpn136 <akbarpn136@gmail.com>
2017-12-01 10:18:15 +0700

    membuat model database untuk tugas serta paket tugas yang diperlukan

--- perelite_core/models.py
+++ perelite_core/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+from mongoengine import *
 
-# Create your models here.
+OPSI_PELAKSANA = (
+    'pendidikan',
+    'kerekayasaan',
+    'profesi',
+    'penunjang',
+)
+
+
+class Lk(EmbeddedDocument):
+    nama = StringField(default='LEMBAR KERJA')
+    kode_peran = StringField(required=True)
+    nomor = StringField(required=True)
+    referensi = StringField(required=True)
+    program = StringField(required=True)
+    wbs_wp = StringField(required=True)
+    uraian_lengkap = StringField(required=True)
+    nama_pemberi = StringField(required=True)
+    peran_pemberi = StringField(required=True)
+
+
+class Tugas(Document):
+    tanggal = DateTimeField(required=True)
+    kategori = StringField(required=True, choices=OPSI_PELAKSANA)
+    butir = StringField(required=True)
+    angka = DecimalField(required=True, min_value=0)
+    satuan = StringField(required=True)
+    uraian_singkat = StringField(required=True)
+    paket_tugas = ListField(GenericEmbeddedDocumentField())
