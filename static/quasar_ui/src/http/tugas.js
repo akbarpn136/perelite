ca128d594f79a3b33dc479950814dcd09833634a
akbarpn136 <akbarpn136@gmail.com>
2017-12-02 10:11:24 +0700

    membuat servis dengan fungsi lihat tugas

--- static/quasar_ui/src/http/tugas.js
+++ static/quasar_ui/src/http/tugas.js
@@ -4,14 +4,24 @@ const URL = process.env.NODE_ENV === 'development' ?
     'http://localhost:8000/' :
     'http://bbta3.bppt.go.id:9601/';
 
-const QWERTY = JSON.parse(localStorage.getItem('qwerty')) || null;
+const QWERTY = JSON.parse(localStorage.getItem('qwerty')) ||
+    JSON.parse({uid: ''});
+const URL_TUGAS = `${URL}core/api1/tugas/`;
 
-function TambahTugas(payload) {
-    return axios.post(`${URL}core/api1/tugas/`, payload, {
+function LihatTugas() {
+    return axios.get(URL_TUGAS, {
         headers: {
             'Authorization': `Bearer ${QWERTY.uid}`
         }
     });
 }
 
-export {TambahTugas};
+function TambahTugas(payload) {
+    return axios.post(URL_TUGAS, payload, {
+        headers: {
+            'Authorization': `Bearer ${QWERTY.uid}`
+        }
+    });
+}
+
+export {LihatTugas, TambahTugas};
