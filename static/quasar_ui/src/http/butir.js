061e81f1d4ffd0344d7ca3262d500584fe34e420
akbarpn136 <akbarpn136@gmail.com>
2017-12-02 10:09:36 +0700

    membuat objek null apabila data offline tidak ada

--- static/quasar_ui/src/http/butir.js
+++ static/quasar_ui/src/http/butir.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8000/' : 'http://bbta3.bppt.go.id:9601/';
 
-const QWERTY = JSON.parse(localStorage.getItem('qwerty')) || null;
+const QWERTY = JSON.parse(localStorage.getItem('qwerty')) ||
+    JSON.parse({uid: ''});
 
 function GetButir(kategori=null) {
     return axios.get(`${URL}utility/api1/butir/${kategori}/`, {
