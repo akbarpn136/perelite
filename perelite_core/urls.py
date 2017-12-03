e443361b39d95f78f75bd9e245e1ad8375784d87
akbarpn136 <akbarpn136@gmail.com>
2017-12-03 11:30:48 +0700

    membuat route baru untuk filter tugas

--- perelite_core/urls.py
+++ perelite_core/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^api1/tugas/', views.Tugas.as_view()),
+    url(r'^api1/tugas/$', views.Tugas.as_view()),
+    url(r'^api1/tugas/(?P<kategori>\w+)/$', views.Tugas.as_view()),
 ]
