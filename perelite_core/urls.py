9e6ca197e598ae577845de8acd069f19d1e514df
akbarpn136 <akbarpn136@gmail.com>
2017-12-07 19:20:30 +0700

    membuat route baru untuk rincian tugas serta modifikasi tugas terkait

--- perelite_core/urls.py
+++ perelite_core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^api1/tugas/$', views.Tugas.as_view()),
     url(r'^api1/tugas/(?P<kategori>\w+)/$', views.Tugas.as_view()),
+    url(r'^api1/tugas/(?P<tugas_id>\w+)/rincian/$', views.TugasModifikasi.as_view()),
 ]
