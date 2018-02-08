32a6c7c5a9a3a88352f2b557da151389f2da98cc
akbarpn136 <akbarpn136@gmail.com>
2018-02-08 16:48:21 +0700

    menambahkan model database untuk tn

--- perelite_core/models.py
+++ perelite_core/models.py
@@ -36,6 +36,20 @@ class Lb(EmbeddedDocument):
     peran_penyetuju = StringField(required=True)
 
 
+class Tn(EmbeddedDocument):
+    nama = StringField(default='TECHNICAL NOTE')
+    nomor = StringField(required=True)
+    kode_peran = StringField(required=True)
+    judul = StringField(required=True)
+    unit_kerja = StringField(required=True)
+    pusat_kerja = StringField(required=True)
+    uraian_lengkap = StringField(required=True)
+    nama_pemeriksa = StringField(required=True)
+    peran_pemeriksa = StringField(required=True)
+    nama_penyetuju = StringField(required=True)
+    peran_penyetuju = StringField(required=True)
+
+
 class Tugas(Document):
     tanggal = DateTimeField(required=True)
     owner = ReferenceField(Personil, reverse_delete_rule=CASCADE)
